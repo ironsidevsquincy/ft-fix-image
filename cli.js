dc8890ed521f2980b352e5fc6716a405e9e1f5a4
Matthew Andrews <matt@mattandre.ws>
2015-03-13 14:32:08 +0000

    Add bin

---
+++ cli.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env iojs
+'use strict';
+
+const program = require('commander');
+const fetch = require('node-fetch');
+const apiKey = require('fs').readFileSync(process.env.HOME + '/.ftapi_v2', { encoding: 'utf8' });
+
+const fetchCapiV2 = function(url) {
+	return fetch(url, { headers: { 'X-Api-Key': apiKey }, timeout: 3000 })
+		.then(function(response) {
+			if (response.ok) {
+				return response.json();
+			}
+			throw new Error("Bad response from CAPI v2 for uuid: ", uuid);
+		});
+}
+
+program
+	.version('1.0.0')
+	.command('article [uuid]')
+	.description('Fix the images within the specified uuid')
+	.action(function(uuid) {
+		fetchCapiV2('http://api.ft.com/content/' + uuid)
+			.then(function(article) {
+				return fetchCapiV2(article.mainImage.id);
+			})
+			.then(function(imageSet) {
+				return fetchCapiV2(imageSet.members[0].id);
+			})
+			.then(function(image) {
+				console.log("http://methode-image-model-transformer-iw-uk-p.svc.ft.com/image/model/" + image.contentOrigin.originatingIdentifier);
+				return fetch("http://binary-ingester-iw-uk-p.svc.ft.com/ingest", {
+					method: 'POST',
+					timeout: 3000,
+					body: '{ "contentUri" : "http://methode-image-model-transformer-iw-uk-p.svc.ft.com/image/model/' + image.contentOrigin.originatingIdentifier + '" }',
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				});
+			})
+			.then(function(response) {
+				if (!response.ok) {
+					console.log(response);
+					throw new Error("Unable to republish image");
+				}
+				console.log("Successfully republished images in " + uuid);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+	});
+
+program
+	.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+	program.outputHelp();
+}
