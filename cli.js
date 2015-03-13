485fbc06325221c73054ddaac138c0038ee15eeb
Matthew Andrews <matt@mattandre.ws>
2015-03-13 15:05:18 +0000

    allll the images

--- cli.js
+++ cli.js
@@ -15,6 +15,27 @@ const fetchCapiV2 = function(url) {
 		});
 }
 
+function republishImageSet(url) {
+	return fetchCapiV2(url)
+		.catch(function(err) {
+			console.log("The imageSet doesn't exist in the Content API so I probably can't fix this. Contact UPPPP");
+			throw err;
+		})
+		.then(function(imageSet) {
+			return fetchCapiV2(imageSet.members[0].id);
+		})
+		.then(function(image) {
+			return fetch("http://binary-ingester-iw-uk-p.svc.ft.com/ingest", {
+				method: 'POST',
+				timeout: 3000,
+				body: '{ "contentUri" : "http://methode-image-model-transformer-iw-uk-p.svc.ft.com/image/model/' + image.contentOrigin.originatingIdentifier + '" }',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+		});
+}
+
 program
 	.version('1.0.0')
 	.command('article [uuid]')
@@ -22,36 +43,27 @@ program
 	.action(function(uuid) {
 		fetchCapiV2('http://api.ft.com/content/' + uuid)
 			.then(function(article) {
-				if (!article.mainImage.id) {
-					throw new Error("Article does not have main image " + uuid);
-				}
-				return fetchCapiV2(article.mainImage.id)
-					.catch(function(err) {
-						console.log("The imageSet doesn't exist in the Content API so I probably can't fix this. Contact UPPPP");
-						throw err;
+				let promises = article.bodyXML.match(/ImageSet" url="http:\/\/api\.ft\.com\/content\/([a-z-0-9]{3,})"/g)
+					.map(function(snippet) {
+						return snippet.replace(/ImageSet" url="(http:\/\/api\.ft\.com\/content\/[a-z-0-9]{3,})"/, "$1");
+					})
+					.map(function(url) {
+						console.log('inline image', url);
+						return republishImageSet(url);
 					});
+				if (article.mainImage.id) {
+					promises.push(republishImageSet(article.mainImage.id));
+				}
+				return Promise.all(promises);
 			}, function(err) {
 				console.log("This article doesn't seem to exist…");
 				throw err;
-			})
-			.then(function(imageSet) {
-				return fetchCapiV2(imageSet.members[0].id);
-			})
-			.then(function(image) {
-				return fetch("http://binary-ingester-iw-uk-p.svc.ft.com/ingest", {
-					method: 'POST',
-					timeout: 3000,
-					body: '{ "contentUri" : "http://methode-image-model-transformer-iw-uk-p.svc.ft.com/image/model/' + image.contentOrigin.originatingIdentifier + '" }',
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				});
 			})
 			.then(function(response) {
 				if (!response.ok) {
 					throw new Error("Unable to republish image");
 				}
-				console.log("Successfully republished images in " + uuid);
+				console.log("Successfully republished images in " + url);
 			})
 			.catch(function(err) {
 				console.log(err);
