278f0fc1775e270ad287542aa2f6b923c5034990
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-25 16:54:18 +0000

    image set and image commands

--- cli.js
+++ cli.js
@@ -27,45 +27,70 @@ const fetchImage = function (url, contentUri) {
     })
 }
 
-function republishImageSet(imageSetUrl) {
-    const imageSetId = imageSetUrl.replace('http://api.ft.com/content/', '');
-	fetchImage('http://semantic-ingester-pr-uk-p.svc.ft.com/ingest', 'image-set/model/' + imageSetId)
+function republishImage(uuid) {
+	return Promise.all([
+	    	fetchImage('http://binary-ingester-iw-uk-p.svc.ft.com/ingest', 'image/model/' + uuid)
+	            .then(function(response) {
+	                if (!response.ok) {
+	                    throw new Error("Unable to republish image");
+	                }
+	                console.log("Successfully republished image '" + uuid + "'");
+	            }),
+	    	fetchImage('http://semantic-ingester-pr-uk-p.svc.ft.com/ingest', 'image/model/' + uuid)
+	            .then(function(response) {
+	                if (!response.ok) {
+	                    throw new Error("Unable to refeed image '" + uuid + "'");
+	                }
+	                console.log("Successfully refed image '" + uuid + "'");
+	            })
+	    ])
+		.catch(function (err) {
+			console.log(err)
+		});
+}
+
+function republishImageSet(uuid) {
+	return fetchImage('http://semantic-ingester-pr-uk-p.svc.ft.com/ingest', 'image-set/model/' + uuid)
         .then(function(response) {
             if (!response.ok) {
-                throw new Error("Unable to refeed image set '" + imageSetId + "'");
+                throw new Error("Unable to refeed image set '" + uuid + "'");
             }
-            console.log("Successfully refed image set '" + imageSetId + "'");
+            console.log("Successfully refed image set '" + uuid + "'");
         })
         .then(function () {
-        	return fetchCapiV2(imageSetUrl)
+        	return fetchCapiV2('http://api.ft.com/content/' + uuid)
         })
-		.catch(function(err) {
-			console.log("The imageSet doesn't exist in the Content API so I probably can't fix this. Contact UPPPP");
-			throw err;
-		})
 		.then(function(imageSet) {
             const imageUpdates = imageSet.members.map(function (image) {
-                const imageId = image.id.replace('http://api.ft.com/content/', '');
-                return [
-                	fetchImage('http://binary-ingester-iw-uk-p.svc.ft.com/ingest', 'image/model/' + imageId)
-	                    .then(function(response) {
-	                        if (!response.ok) {
-	                            throw new Error("Unable to republish image");
-	                        }
-	                        console.log("Successfully republished image '" + imageId + "'");
-	                    }),
-                	fetchImage('http://semantic-ingester-pr-uk-p.svc.ft.com/ingest', 'image/model/' + imageId)
-	                    .then(function(response) {
-	                        if (!response.ok) {
-	                    		console.log('ERROR')
-	                            throw new Error("Unable to refeed image '" + imageId + "'");
-	                        }
-	                        console.log("Successfully refed image '" + imageId + "'");
-	                    })
-                ];
+                return republishImage(image.id.replace('http://api.ft.com/content/', ''));
             });
-			return Promise.all(flatten(imageUpdates));
+			return Promise.all(imageUpdates);
+		})
+		.catch(function (err) {
+			console.log(err)
 		});
+}
+
+function republishArticle(uuid) {
+	fetchCapiV2('http://api.ft.com/content/' + uuid)
+		.then(function(article) {
+			const imageIds = article.bodyXML.match(/ImageSet" url="http:\/\/api\.ft\.com\/content\/([a-z-0-9]{3,})"/g)
+				.map(function(snippet) {
+					return snippet.replace(/ImageSet" url="(http:\/\/api\.ft\.com\/content\/[a-z-0-9]{3,})"/, "$1");
+				});
+			if (article.mainImage.id && imageIds.indexOf(article.mainImage.id) === -1) {
+				imageIds.push(article.mainImage.id);
+			}
+			let promises = imageIds.map(function(url) {
+				console.log('inline image', url);
+				const imageSetId = url.replace('http://api.ft.com/content/', '');
+				return republishImageSet(imageSetId);
+			});
+			return Promise.all(promises);
+		})
+		.catch(function(err) {
+			console.log(err);
+		})
 }
 
 program
@@ -73,27 +98,21 @@ program
 	.command('article [uuid]')
 	.description('Fix the images within the specified uuid')
 	.action(function(uuid) {
-		fetchCapiV2('http://api.ft.com/content/' + uuid)
-			.then(function(article) {
-				const imageIds = article.bodyXML.match(/ImageSet" url="http:\/\/api\.ft\.com\/content\/([a-z-0-9]{3,})"/g)
-					.map(function(snippet) {
-						return snippet.replace(/ImageSet" url="(http:\/\/api\.ft\.com\/content\/[a-z-0-9]{3,})"/, "$1");
-					});
-				if (article.mainImage.id && imageIds.indexOf(article.mainImage.id) === -1) {
-					imageIds.push(article.mainImage.id);
-				}
-				let promises = imageIds.map(function(url) {
-					console.log('inline image', url);
-					return republishImageSet(url);
-				});
-				return Promise.all(promises);
-			}, function(err) {
-				console.log("This article doesn't seem to exist…");
-				throw err;
-			})
-			.catch(function(err) {
-				console.log(err);
-			});
+		republishArticle(uuid);
+	});
+
+program
+	.command('image-set [uuid]')
+	.description('Fix an image set and its images')
+	.action(function(uuid) {
+		republishImageSet(uuid);
+	});
+
+program
+	.command('image [uuid]')
+	.description('Fix a media resource metadata and image')
+	.action(function(uuid) {
+		republishImage(uuid);
 	});
 
 program
